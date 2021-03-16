2d24be18c2e904127d71ff278ca89a54c6442ec7
郑柏屹 <ban11111@qq.com>
2021-03-16 21:16:47 +0800

    test

---
+++ precache-manifest.4a4a51026c29a0483c728380e7830dab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab969d0cf09f237c8165668e00e5f185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0865ce3fccebd0fde15",
+    "url": "/static/css/2.1be17d03.chunk.css"
+  },
+  {
+    "revision": "95a7b8b1ae63fc2cb047",
+    "url": "/static/css/3.9b7e5df8.chunk.css"
+  },
+  {
+    "revision": "f0d6a5f70d0c5708e182",
+    "url": "/static/css/main.d1ec6c11.chunk.css"
+  },
+  {
+    "revision": "b0865ce3fccebd0fde15",
+    "url": "/static/js/2.788856d3.chunk.js"
+  },
+  {
+    "revision": "475eb4a01b5298d6a1623ea054f813d3",
+    "url": "/static/js/2.788856d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a7b8b1ae63fc2cb047",
+    "url": "/static/js/3.0367c00a.chunk.js"
+  },
+  {
+    "revision": "0d469caf17fb6b0ac152",
+    "url": "/static/js/4.026d3c48.chunk.js"
+  },
+  {
+    "revision": "f0d6a5f70d0c5708e182",
+    "url": "/static/js/main.57fc0985.chunk.js"
+  },
+  {
+    "revision": "6c78858742bc4bead2c0",
+    "url": "/static/js/runtime-main.f75a85c4.js"
+  }
+]);
