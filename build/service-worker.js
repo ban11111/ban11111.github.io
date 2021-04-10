a565d7a78be5971b6cf022a102749921ed885355
“郑柏屹” <zhengbaiyi@mintechai.com>
2021-04-10 18:15:13 +0800

    3

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f077e722a451b7def092af10c13fdc07.js"
+  "/precache-manifest.64dc7cf2c8c300fd76cf8a63c685b49d.js"
 );
 
 self.addEventListener('message', (event) => {
