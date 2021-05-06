c4d805cc33c71b2f5f2be2fbfdcd63a89ac533b0
“郑柏屹” <zhengbaiyi@mintechai.com>
2021-05-06 23:58:28 +0800

    asdfdsf

---
+++ build/precache-manifest.621ae9cef0876f4d3e86a7ca844f770e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2261a23e0093844907fadceaa63d5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe536bd556357a81af1c",
+    "url": "/static/css/2.2a201987.chunk.css"
+  },
+  {
+    "revision": "8db3e9375768d009dcb1",
+    "url": "/static/css/main.bf985735.chunk.css"
+  },
+  {
+    "revision": "fe536bd556357a81af1c",
+    "url": "/static/js/2.8f1d9e53.chunk.js"
+  },
+  {
+    "revision": "36f2b7b2110ab20b60768e1a937d0d38",
+    "url": "/static/js/2.8f1d9e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f530fa227f22fd1a23c9",
+    "url": "/static/js/3.de28dd63.chunk.js"
+  },
+  {
+    "revision": "8db3e9375768d009dcb1",
+    "url": "/static/js/main.eb457da0.chunk.js"
+  },
+  {
+    "revision": "b5ae25592af293d81a2b",
+    "url": "/static/js/runtime-main.80b7b6ac.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cffcb0ca9efa9139a5797aae6e33cc7.js"
+  "/precache-manifest.621ae9cef0876f4d3e86a7ca844f770e.js"
 );
 
 self.addEventListener('message', (event) => {
