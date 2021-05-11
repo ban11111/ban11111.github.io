e3b452beeaab7d3d61fa9cd2938294b753efae43
郑柏屹 <ban11111@qq.com>
2021-05-11 17:37:44 +0800

    fds456af415dsa6

---
+++ build/precache-manifest.0b5e9ecfe183e11606d9968b2c425b46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000eae93494afe8500659462a8d9e4d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d40ad9be91e61cfe4b2b",
+    "url": "/static/css/2.540e5144.chunk.css"
+  },
+  {
+    "revision": "c5555175568af1b58bd3",
+    "url": "/static/css/main.527420b1.chunk.css"
+  },
+  {
+    "revision": "d40ad9be91e61cfe4b2b",
+    "url": "/static/js/2.98c5c747.chunk.js"
+  },
+  {
+    "revision": "36f2b7b2110ab20b60768e1a937d0d38",
+    "url": "/static/js/2.98c5c747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ff75d59e49102b9133",
+    "url": "/static/js/3.d9d477b3.chunk.js"
+  },
+  {
+    "revision": "c5555175568af1b58bd3",
+    "url": "/static/js/main.1a73bf58.chunk.js"
+  },
+  {
+    "revision": "6f641f2e1e5c03140e6e",
+    "url": "/static/js/runtime-main.3909e89a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ea66fa4ae6beaa3f73c5cc896932958.js"
+  "/precache-manifest.0b5e9ecfe183e11606d9968b2c425b46.js"
 );
 
 self.addEventListener('message', (event) => {
